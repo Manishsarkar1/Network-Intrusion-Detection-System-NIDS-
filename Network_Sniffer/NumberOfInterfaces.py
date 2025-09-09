56dae0cfad5b72d351cee336031d3432addd55cf
Manish Sarkar <sarkarmrigank28@gmail.com>
2025-09-10 00:15:32 +0530

    fixed code for linux

--- Network_Sniffer/NumberOfInterfaces.py
+++ Network_Sniffer/NumberOfInterfaces.py
@@ -1,2 +1,9 @@
 from scapy.all import get_if_list
-print(get_if_list)
+
+interfaces_list = get_if_list()
+
+num_interfaces = len(interfaces_list)
+print(f"Number of Network Interfaces: {num_interfaces}")
+
+print("\nList of interface names: ")
+print(interfaces_list)
